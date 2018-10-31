80139a05b6c2ba1cb24d0214429cfcc70a9c1be1
rajhaq <zubaer.haque@gmail.com>
2018-10-31 23:03:58 +0600

    return on going

--- resources/assets/js/components/return/returnproduct.vue
+++ resources/assets/js/components/return/returnproduct.vue
@@ -14,8 +14,8 @@
                                         </Col>
                                         <Col span="12">
                                         <p>Product Code</p>
-                                            <Input type="text" placeholder="Barcode" @on-enter="addProduct" 
-                                            v-model="formValue.barCode"></Input>  
+                                            <Input type="text" placeholder="Barcode" @on-enter="addNewSale" 
+                                            v-model="formValue.productCode"></Input>  
                                         </Col>
                                     </Row>
                         </Card>
@@ -45,12 +45,25 @@
                                     <td><input type="number" v-model="data.discountedPrice*data.quantity" disabled></input></td>
                                     <td><Button type="error" icon="ios-trash" @click="removeItem(i)"></Button></td>
                                 </tr>
-
+                                <tr v-for="(data,i) in formValue.newProduct" :key="i">
+                                    <td >{{data.productName}}</td>
+                                    <td >{{data.model}}</td>
+                                    <td >{{data.color}}</td>
+                                    <td>{{data.size}}</td>
+                                    <td>{{data.sellingPrice}}<Tag  color="red" v-if="data.discount" type="border">-{{data.discount}}%</Tag></td>
+                                    <td><InputNumber :min="0" :max="data.stock" v-model="data.quantity" @on-change="quantityChange"></InputNumber></td>
+                                    <td><input type="number" v-model="data.discountedPrice*data.quantity" disabled></input></td>
+                                    <td><Button type="error" icon="ios-trash" @click="removeItemNew(i)"></Button></td>
+                                </tr>
                                 <tr style="background-color: #e9eaec;" >
                                     <td colspan="5" style="text-align:right;">Sub Total </td>
                                     <td >{{formValue.totalQuantity}}</td>
                                     <td colspan="2">{{formValue.subTotal}}</td>
                                     
+                                </tr>
+                                <tr v-if="formValue.oldTotal>0">
+                                    <td colspan="7" style="text-align:right">Previous Paid Amound</td>
+                                    <td><InputNumber  :min="0" :max="formValue.oldTotal" @on-change="total" v-model="formValue.oldTotal"></InputNumber ></td>
                                 </tr>
                                 <tr >
                                     <td colspan="7" style="text-align:right">Discount</td>
@@ -142,13 +155,17 @@
                      type:'purchase',
                      date:'',
                      discount:0,
+                     oldTotal:0,
                      paidAmount:0,
                      subTotal:0,
                      subQuantity:0,
                      total:0,
                      supplier_id: '',
                      customer_id: '',
-                     productDetails: []
+                     productDetails: [],
+                     newProductFlag: true,
+                     newProduct: [],
+                     productCode:''
                 },
                
             }
@@ -211,21 +228,23 @@
                     }
                 this.clearData()
             },
-            async addProduct()
+            async addNewSale()
             {
-                if(this.formValue.barCode)
+               
+
+                if(this.formValue.productCode)
                 {
-                    if(this.formValue.productDetails)
+                    if(this.formValue.newProduct)
                     {
-                    for(let d of this.formValue.productDetails)
+                    for(let d of this.formValue.newProduct)
                     {
-                        if(d.barCode == this.formValue.barCode){
+                        if(d.barCode == this.formValue.productCode){
                             if(d.stock==d.quantity){
                                 return this.i('You have acceded the available stock')
                             }
                             d.quantity++
                             this.quantityChange()
-                            this.formValue.barCode=null
+                            this.formValue.productCode=null
                              return 
 
                             }
@@ -236,7 +255,7 @@
                 try{
                 let {data} =await axios({
                     method: 'get',
-                    url:`/app/searchProduct/${this.formValue.barCode}`,
+                    url:`/app/searchProduct/${this.formValue.productCode}`,
                     })
                     let ps=0,ss=0
                     console.log(data)
@@ -252,35 +271,34 @@
 
 
                     if(data.sell_stock){
-                        console.log('IU am')
+                        console.log('11')
                         ss=data.sell_stock.stock
                     }
                     data.stock=ps-ss
                     data.quantity=1
                     for(let d of this.dataGroup){
-                        console.log('IU am')
+                        console.log('12')
                         if(d.groupName==data.groupName){
-                        console.log('IU am')
+                        console.log('22')
                             data.discount=d.discount
                         }
                     }
                    if(data.discount){
-                        console.log('IU am')
+                        console.log('33')
                         let d= (data.discount*data.sellingPrice)/100
                         data.discountedPrice= data.sellingPrice-d
                    }else{
-                        console.log('IU am')
+                        console.log('44')
                          data.discountedPrice= data.sellingPrice
                          data.discount=0
                    }
 
-                    this.formValue.productDetails.push(data)
+                    this.formValue.newProduct.push(data)
                     this.quantityChange()
                     this.lf();
-                    this.formValue.barCode=null
+                    this.formValue.productCode=null
 
                 }catch(e){
-                    console.log(0)
                     this.e('Oops!','3Something went wrong, please try again!')
                     this.le();
                 }
@@ -291,18 +309,33 @@
                 this.formValue.productDetails[index].quantity=0
                 this.quantityChange()
             },
+            removeItemNew(index)
+            {
+                 this.formValue.newProduct.splice(index,1)
+                this.quantityChange()
+            },
             quantityChange()
             {
+                if(this.formValue.newProductFlag)
+                {
+                this.formValue.oldTotal=this.formValue.paidAmount
+                this.formValue.newProductFlag=false
+
+                }
                 
-                var totalPrice=0,totalQuantity=0
+                var totalPrice=0,totalQuantity=0,totalQuantityN=0,totalPriceN=0
                 for ( let d of this.formValue.productDetails) {
                         totalQuantity+=parseInt(d.quantity)               
                         totalPrice+=(d.quantity*d.discountedPrice)
                     }
-                totalPrice=Math.round(totalPrice).toFixed(2)
-                this.formValue.totalQuantity=totalQuantity
+                for ( let d of this.formValue.newProduct) {
+                        totalQuantityN+=parseInt(d.quantity)               
+                        totalPriceN+=(d.quantity*d.discountedPrice)
+                    }
+                totalPrice=Math.round(totalPrice+totalPriceN).toFixed(2)
+                this.formValue.totalQuantity=Math.round(totalQuantity+totalQuantityN).toFixed(2)
                 this.formValue.total=parseFloat(totalPrice)
-                this.formValue.paidAmount=parseFloat(totalPrice)
+                this.formValue.paidAmount=parseFloat(totalPrice-this.formValue.oldTotal)
                 this.formValue.subTotal=parseFloat(totalPrice)
                 this.discount()
 
@@ -313,8 +346,8 @@
                 var discountAmount = (this.formValue.discount*this.formValue.subTotal)/100
                 var afterDiscount = totalOld - discountAmount
                 afterDiscount= Math.round(afterDiscount).toFixed(2)
-                this.formValue.total=afterDiscount
-                this.formValue.paidAmount=afterDiscount
+                this.formValue.total=parseFloat(afterDiscount)
+                this.formValue.paidAmount=parseFloat(afterDiscount-this.formValue.oldTotal)
             },
             total(){
                 var totalOld = this.formValue.subTotal
@@ -385,7 +418,7 @@
                             this.formValue.total=this.dataSearch.sellingPrice
                             this.formValue.discount=this.dataSearch.discount
                             this.formValue.date=this.dataSearch.date
-
+                            this.quantityChange()
                         }catch(e){
                             this.e('Oops!','Something went wrong, please try again!')
                             this.le()
@@ -484,7 +517,18 @@
                 this.e('Oops!','Something went wrong, please try again!')
             this.le();
             }
+            try{
+                let {data} =await  axios({
+                    method: 'get',
+                    url:'/app/group'
+                })
+                this.dataGroup=data;
+                this.lf();
 
+            }catch(e){
+                this.e('Oops!','Something went wrong, please try again!')
+            this.le();
+            }
         }
 
     }
