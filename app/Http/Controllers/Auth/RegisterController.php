0d680efcfd65a0ccd6d02ba4f137233a7ed2d017
rajhaq <zubaer.haque@gmail.com>
2018-11-04 19:52:45 +0600

    registration bug

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -67,7 +67,6 @@ class RegisterController extends Controller
             
             'name' => $data['name'],
             'email' => $data['email'],
-            'userType' => $data['userType'],
             'password' => Hash::make($data['password']),
         ]);
     }
